6c71900d3f23184a482c7ead36bc46179e8377d4
Mathias Andresen <mathias@3mogs.dk>
2021-11-25 18:32:07 +0100

    feat: update index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Card from '../components/card.svelte';
+  import exampleCards from '../../static/rpg-cards_many.json';
+  import 'ress/dist/ress.min.css';
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import { parseCard } from '../lib/card-json-parser';
+  import { Container, Row, Col, Navbar, NavbarBrand } from 'sveltestrap';
+  import Sidebar from '../components/sidebar.svelte';
+  import CardEditor from '../components/card-editor.svelte';
+  import { deck } from '../stores';
+
+  const cards = exampleCards.map((json) => parseCard(JSON.stringify(json)));
+
+  deck.set(cards);
+</script>
+
+<Navbar>
+  <NavbarBrand href="/">RPG Card Generator</NavbarBrand>
+</Navbar>
+<Container fluid>
+  <Row cols={3}>
+    <Col sm={3}>
+      <Sidebar />
+    </Col>
+    <Col>
+      <CardEditor />
+    </Col>
+    <Col>hello</Col>
+  </Row>
+</Container>
