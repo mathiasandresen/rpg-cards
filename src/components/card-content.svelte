5167d366bdffd6c244a9fc1292c74308cbaeeec1
Mathias Andresen <mathias@3mogs.dk>
2021-11-25 18:30:05 +0100

    feat: create card module

---
+++ src/components/card-content.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+	import type { CardContent } from '../model/card';
+
+	export let content: CardContent;
+</script>
+
+{#if content?.type === 'subtitle'}
+	<h2>{content.content}</h2>
+{:else if content?.type === 'rule'}
+	<div class="rule" />
+{:else if content?.type === 'section'}
+	<h3 class="section">{content.content}</h3>
+{:else if content?.type === 'property'}
+	<span class="property">
+		{#each content.content.split(' | ') as element}
+			<p>{element}</p>
+		{/each}
+	</span>
+{:else if content?.type === 'text'}
+	<p class="text">{content.content}</p>
+{:else}
+	<p>{content.content}</p>
+{/if}
+
+<style lang="scss">
+	$section-height: 0.3cm;
+
+	p {
+		padding: 0 0.5em;
+		margin-bottom: 0.2em;
+		font-size: 1em;
+		line-height: 1em;
+	}
+
+	h2 {
+		font-size: 1.2em;
+	}
+
+	.section {
+		background-color: red;
+		display: flex;
+		place-content: center;
+		place-items: center;
+		color: white;
+		height: $section-height;
+		font-size: 0.8em;
+		margin-bottom: 0.2em;
+		margin-left: -2px;
+		margin-right: -2px;
+	}
+
+	.property {
+		display: block;
+		text-indent: -1em;
+		margin-left: 1em;
+		margin-bottom: 0.2em;
+		padding: 0 0.5em;
+
+		p {
+			display: inline;
+			padding: unset;
+		}
+
+		:first-child {
+			font-weight: bold;
+			margin-right: 0.3em;
+		}
+
+		:global + .property {
+			margin-top: -0.5em;
+		}
+	}
+
+	.rule {
+		border-bottom: 2px solid red;
+		margin-bottom: 0.2em;
+		margin-left: -2px;
+		margin-right: -2px;
+	}
+
+	.text {
+		font-size: 1em;
+	}
+</style>
---
+++ src/components/card.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import type Card from '../model/card';
+	import CardContent from './card-content.svelte';
+
+	export let card: Card;
+</script>
+
+<div class="rpg-card-wrapper">
+	<div class="rpg-card">
+		<h1>{card.title}</h1>
+		{#each card.contents as content}
+			<CardContent {content} />
+		{/each}
+	</div>
+</div>
+
+<style lang="scss">
+	$border-width: 0.3cm;
+
+	h1 {
+		margin: 0;
+	}
+
+	.rpg-card-wrapper {
+		width: 2.5in;
+		height: 3.5in;
+
+		box-sizing: border-box;
+		border: $border-width solid red;
+		border-radius: 0.25cm;
+		background-color: red;
+	}
+
+	.rpg-card {
+		// background-color: rgb(230, 114, 114);
+
+		display: flex;
+		flex-direction: column;
+		// gap: 0.2rem;
+		height: 100%;
+
+		font-size: 10px;
+
+		border-radius: 0.1cm;
+		background-color: white;
+
+		h1 {
+			font-size: 1.3em;
+			text-transform: uppercase;
+			text-align: center;
+			font-weight: bold;
+		}
+	}
+</style>
