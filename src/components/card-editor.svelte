d51f7db387d409813c72098714ea6b5a70597e4b
Mathias Andresen <mathias@3mogs.dk>
2021-11-28 16:07:22 +0100

    feat: implementing more of card editor

--- src/components/card-editor.svelte
+++ src/components/card-editor.svelte
@@ -1,10 +1,28 @@
 <script lang="ts">
   import { Form, FormGroup, Input, Label } from 'sveltestrap';
+  import { currentCard, deck } from '../stores';
+
+  const card = $deck[$currentCard];
+  $: console.log(card);
 </script>
 
 <Form>
-  <FormGroup>
-    <Input id="" placeholder="Paper" />
-    <Label for="exampleEmail">Hello</Label>
+  <FormGroup row>
+    <Label class="col-sm-2 col-form-label" for="name">Name</Label>
+    <div class="col-sm-10">
+      <Input type="text" name="name" id="name" bind:value={card.title} placeholder="Name" />
+    </div>
+  </FormGroup>
+  <FormGroup row>
+    <Label class="col-sm-2 col-form-label" for="count">Count</Label>
+    <div class="col-sm-10">
+      <Input type="number" name="name" id="name" bind:value={card.count} placeholder="Count" />
+    </div>
+  </FormGroup>
+  <FormGroup row>
+    <Label class="col-sm-2 col-form-label" for="count">Icon</Label>
+    <div class="col-sm-10">
+      <Input type="text" name="name" id="name" bind:value={card.icon} placeholder="Icon" />
+    </div>
   </FormGroup>
 </Form>
