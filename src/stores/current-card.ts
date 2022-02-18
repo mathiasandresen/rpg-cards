5b08ffb568930e1473d8150d8ed0239732b14a8b
Mathias Andresen <mathias@3mogs.dk>
2022-02-18 16:06:51 +0100

    feat: add duplicate button

--- src/stores/current-card.ts
+++ src/stores/current-card.ts
@@ -1,12 +1,22 @@
 import { browser } from '$app/env';
 import { writable } from 'svelte/store';
 
-let currentCardDefaultValue = 0;
-if (browser) {
-  currentCardDefaultValue = Number.parseInt(localStorage?.getItem('currentCard')) ?? 0;
+function createCurrentCard() {
+  let currentCardDefaultValue = 0;
+  if (browser) {
+    currentCardDefaultValue = Number.parseInt(localStorage?.getItem('currentCard')) ?? 0;
+  }
+
+  const { subscribe, set, update } = writable<number>(currentCardDefaultValue);
+
+  return {
+    subscribe,
+    set,
+    update
+  };
 }
 
-export const currentCard = writable<number>(currentCardDefaultValue);
+export const currentCard = createCurrentCard();
 
 currentCard.subscribe((currentCard) => {
   if (browser) {
